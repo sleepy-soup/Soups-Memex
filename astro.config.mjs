dafedf3716a5d9e82699ade5b9a9b49d3ddbd9c8
sleepy-soup <colinsoup51@gmail.com>
2026-07-18 19:31:52 -0400

    Set up GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
+const repository = process.env.GITHUB_REPOSITORY?.split('/')[1];
+
 export default defineConfig({
+  site: 'https://sleepy-soup.github.io',
+  // GitHub Actions supplies the repository name during deployment. Locally,
+  // the site continues to run at the root of localhost.
+  base: repository ? `/${repository}` : '/',
   integrations: [tailwind({ applyBaseStyles: false })],
 });
